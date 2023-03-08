1c040a8d3240957e9c16274031026a62238c0f24
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-08 13:36:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -4,17 +4,5 @@ function introduction(Edwin) {
   function introductionWithLanguage(Edwin, Python ) {
     return `Hi, my name is ${Edwin} and I am learning to program in ${Python}.`;
   }
-  function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
-  function introduction(Edwin) {
-    return `Hi, my name is ${Edwin}.`;
-  }
-  function introductionWithLanguage(Edwin, Python ) {
-    return `Hi, my name is ${Edwin} and I am learning to program in ${Python}.`;
-  }
-  function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
   
   
